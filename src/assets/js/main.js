de8965918933e7f5eb1986b7db29a50c6b73cf34
Tubbo <65963015+Tubbster-UX@users.noreply.github.com>
2022-07-19 15:36:28 -0600

    First Update

---
+++ src/assets/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#navIcon').click(function() {
+    $('#navContent').toggleClass('block').toggleClass('hidden');
+  })
+})
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ["./src/**/*.{html,css,js}"],
+  content: ["./src/**/*.{html,css,js}"],
+  theme: {
+    extend: {},
+  },
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#ca9ef7",
+        
+"secondary": "#d8b4fe",
+        
+"accent": "#37CDBE",
+        
+"neutral": "#3D4451",
+        
+"base-100": "#FFFFFF",
+        
+"info": "#3ABFF8",
+        
+"success": "#36D399",
+        
+"warning": "#FBBD23",
+        
+"error": "#F87272",
+        },
+      },
+    ],
+  },
+  
+  variants: {},
+  plugins: [require("daisyui")],
+};
