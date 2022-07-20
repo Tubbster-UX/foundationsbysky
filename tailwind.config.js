bc386ddab2f47a51ecae1b5db932951ddf8770a2
Tubbo <Alexavierbrink@gmail.com>
2022-07-19 23:46:28 -0600

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,36 +6,40 @@ module.exports = {
   purge: ["./src/**/*.{html,css,js}"],
   content: ["./src/**/*.{html,css,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'book-now-image': "url('../img/massage.jpg')",
+      }
+    },
   },
 
   daisyui: {
     themes: [
       {
         mytheme: {
-        
-"primary": "#ca9ef7",
-        
-"secondary": "#d8b4fe",
-        
-"accent": "#37CDBE",
-        
-"neutral": "#3D4451",
-        
-"base-100": "#FFFFFF",
-        
-"info": "#3ABFF8",
-        
-"success": "#36D399",
-        
-"warning": "#FBBD23",
-        
-"error": "#F87272",
+
+          "primary": "#ca9ef7",
+
+          "secondary": "#d8b4fe",
+
+          "accent": "#37CDBE",
+
+          "neutral": "#3D4451",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
         },
       },
     ],
   },
-  
+
   variants: {},
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require('@tailwindcss/aspect-ratio')],
 };
